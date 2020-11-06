82bde8fe8c51f2bd07e372efd0064ab483f622d5
Dillon Mulroy <dillon.mulroy@formidable.com>
2020-11-05 22:09:46 -0500

    wip

--- src/diagnostics-dashboard.ts
+++ src/diagnostics-dashboard.ts
@@ -1,14 +1,33 @@
-import dc, { Channel } from 'diagnostics_channel';
+import { Channel } from 'diagnostics_channel';
+
+interface ChannelData<T> {
+  channel: Channel<T>;
+  handler: (message: T, channelName: string | symbol) => void;
+}
 
 export default class DiagnosticsDashboard {
   constructor(
-    private readonly channels: Map<string | symbol, Channel<any>> = new Map()
+    private readonly channels: Map<
+      string | symbol,
+      ChannelData<any>
+    > = new Map()
   ) {}
-  subscribe<T>(channel: Channel<T>): void {
-    this.channels.set(channel.name, channel);
 
-    channel.subscribe((message, channelName) => {
+  subscribe<T>(channel: Channel<T>): void {
+    const handler = (message: T, channelName: string | symbol): void => {
       console.log({ channelName, message });
-    });
+    };
+
+    this.channels.set(channel.name, { channel, handler });
+
+    channel.subscribe(handler);
+  }
+
+  unsubscribe(channelName: string | symbol): void {
+    if (this.channels.has(channelName)) {
+      const { channel, handler } = this.channels.get(channelName)!;
+
+      channel.unsubscribe(handler);
+    }
   }
 }
